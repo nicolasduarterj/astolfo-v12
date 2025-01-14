8517fccba6e83e7a7dd5178d769f5c692c5cc0a2
Nicolas Duarte <nicolas_duarterj@proton.me>
2025-01-13 22:37:45 -0300

    add sair da party

---
+++ commands/Party/sairParty.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('discord.js');
+const PlayerCharacter = require('../../models/playerCharacter');
+
+const commandData = new SlashCommandBuilder()
+                            .setName('sairparty')
+                            .setDescription('Remove o seu personagem atual da sua party');
+
+async function execute(interaction) {
+    await interaction.reply('Processando...');
+    const currentChar = await PlayerCharacter.findOne({ ownerUUID: interaction.user.id, worn: true });
+    if (!currentChar) {
+        await interaction.editReply('```diff\n-Você não está jogando com um personagem, use /jogarcomo```');
+        return;
+    }
+    currentChar.partyID = null;
+    try {
+        await currentChar.save();
+        await interaction.editReply('```fix\n' + `${currentChar.name} saiu da party!` + '\n```');
+    } catch (error) {
+        console.log(error);
+        await interaction.editReply('```diff\n- Houve um erro inesperado\n```');
+    }
+}
+
+module.exports = {
+    data: commandData,
+    execute: execute
+};
