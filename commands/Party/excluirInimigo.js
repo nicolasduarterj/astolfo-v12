230ecc87aff7ad98e16fa13d8bcb49d70b2e4b41
Nicolas Duarte <nicolas_duarterj@proton.me>
2025-01-14 12:56:55 -0300

    add excluir inimigos

---
+++ commands/Party/excluirInimigo.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder } = require('discord.js');
+const Enemy = require('../../models/inimigo');
+
+const commandData = new SlashCommandBuilder()
+                            .setName('excluirinimigo')
+                            .setDescription('Exclui um inimigo')
+                            .addStringOption(option =>
+                                option
+                                    .setName('name')
+                                    .setDescription('Nome do inimigo a ser excluído')
+                                    .setRequired(true)
+                                    .setAutocomplete(true));
+
+async function autocomplete(interaction) {
+    const enemies = await Enemy.find({ ownerUUID: interaction.user.id });
+    await interaction.respond(enemies.map(enemy => ({ name: enemy.name, value: enemy.name })));
+}
+
+async function execute(interaction) {
+    await interaction.reply({ content: 'Processando...', ephemeral: true });
+    const enemyName = interaction.options.getString('name');
+    try {
+        await Enemy.findOneAndDelete({ ownerUUID: interaction.user.id, name: enemyName });
+        await interaction.editReply('```fix\nInimigo deletado\n```');
+    } catch (error) {
+        console.log(error);
+        await interaction.editReply('```diff\n- Houve um erro inesperado\n```');
+    }
+    return;
+}
+
+module.exports = {
+    data: commandData,
+    autocomplete: autocomplete,
+    execute: execute
+};
